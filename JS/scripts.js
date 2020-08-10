7de1408c622ec7fc1e23b1ea659719df291af4c7
Mark Tiapkov <mark.tiapkov@gmail.com>
2020-08-10 13:49:01 -0700

    Create calculator functions
    Co-authored-by: Jeff Dinsmore <hello@jeffdinsmore.com>

---
+++ JS/scripts.js
@@ -0,0 +1,10 @@
+
+function subtract(number1, number2) {
+  return number1 - number2;
+}
+
+const number1 = parseInt(prompt("Enter a number:"));
+const number2 = parseInt(prompt("Enter a number:"));
+
+alert(subtract(number1, number2));
+alert(result);
