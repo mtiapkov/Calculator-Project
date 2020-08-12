9c743e66e0a5f5b468cf91bb12a7d0728252f63d
Mark Tiapkov <mark.tiapkov@gmail.com>
2020-08-12 11:28:46 -0700

    linked css/js
    Co-authored-by: Alex Goodwin <hagoodwi@gmail.com>

--- JS/scripts.js
+++ JS/scripts.js
@@ -1,4 +1,16 @@
 
+//addition
+function add(number1, number2) {
+  return number1 + number2;
+}
+
+const number1 = parseInt(prompt("Enter a number:"));
+const number2 = parseInt(prompt("Enter another number:"));
+
+alert(add(number1, number2));
+alert(result);
+
+//subtraction
 function subtract(number1, number2) {
   return number1 - number2;
 }
@@ -7,4 +19,26 @@ const number1 = parseInt(prompt("Enter a number:"));
 const number2 = parseInt(prompt("Enter a number:"));
 
 alert(subtract(number1, number2));
+alert(result);
+
+//multiplication
+function multiplication(number1, number2) {
+  return number1 * number2;
+}
+
+const number1 = parseInt(prompt("Enter a number:"));
+const number2 = parseInt(prompt("Enter a number:"));
+
+alert(multiplication(number1, number2));
+alert(result);
+
+//Division
+function division(number1, number2) {
+  return number1 / number2;
+}
+
+const number1 = parseInt(prompt("Enter a number:"));
+const number2 = parseInt(prompt("Enter a number:"));
+
+alert(division(number1, number2));
 alert(result);
