7fbd56fb7d9b591ab454273b5823d520d56b88fd
Mark Tiapkov <mark.tiapkov@gmail.com>
2020-08-12 13:36:33 -0700

    Add HTML and interface logic for division, multiplication and subtraction
    Co-authored-by: Alex Goodwin <hagoodwi@gmail.com>

--- JS/scripts.js
+++ JS/scripts.js
@@ -1,4 +1,3 @@
-
 // Business logic:
 
 function add(number1, number2) {
@@ -20,12 +19,34 @@ function divide(number1, number2) {
 
 
 //Interface Logic
+
 $(document).ready(function() {
   $("form#add").submit(function(event) {
     event.preventDefault();
     const number1 = parseInt($("#add1").val());
     const number2 = parseInt($("#add2").val());
     const result = add(number1, number2);
+    $(".output").text(result);
+  });
+  $("form#subtract").submit(function(event){
+    event.preventDefault();
+    const number1 = parseInt($("#subtract1").val());
+    const number2 = parseInt($("#subtract2").val());
+    const result = subtract(number1, number2);
+    $(".output").text(result);
+  });
+  $("form#multiply").submit(function(event){
+    event.preventDefault();
+    const number1 = parseInt($("#multiply1").val());
+    const number2 = parseInt($("#multiply2").val());
+    const result = multiply(number1, number2);
+    $(".output").text(result);
+  });
+  $("form#divide").submit(function(event){
+    event.preventDefault();
+    const number1 = parseInt($("#divide1").val());
+    const number2 = parseInt($("#divide2").val());
+    const result = divide(number1, number2);
     $(".output").text(result);
   });
 });
